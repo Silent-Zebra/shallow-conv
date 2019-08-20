5e92b8638c72957594196a0e26809e5f72aa7cb3
Stephen Zhao <szstephenzhao88@gmail.com>
2019-08-20 14:37:51 -0400

    Sync

--- Experiments_CIFAR100.py
+++ Experiments_CIFAR100.py
@@ -7,22 +7,23 @@ import pickle
 
 downsampled_size = 9
 
-batch_size = 128
+batch_size = 1000
 
 # margin for triplet loss function
 margin = 2.
 
-n_epochs = 100
+n_epochs = 200
 # log every x batches
-log_interval = 20
+log_interval = 10
 
 # Convnet hyperparameters
-lr = 1e-3
+lr = 1e-4
 input_depth = 3
 layer1_stride = 1
 layer1_kernel_size = 8
 layer1_output_channels = 64
 layer1_padding = 0
+use_relu = True
 
 visualize_filter = True
 
@@ -36,24 +37,24 @@ import utils
 from datasets import DownsampledCIFAR100
 import torch
 
-
 train_dataset = CIFAR100('./data/CIFAR100', train=True, download=True,
                              transform=transforms.Compose([
+                                 transforms.RandomCrop(downsampled_size),
+                                 transforms.RandomHorizontalFlip(),
                                  transforms.ToTensor(),
                              ]))
 test_dataset = CIFAR100('./data/CIFAR100', train=False, download=True,
                             transform=transforms.Compose([
+                                transforms.RandomCrop(downsampled_size),
+                                transforms.RandomHorizontalFlip(),
                                 transforms.ToTensor(),
                             ]))
 
-downsampled_train_dataset = DownsampledCIFAR100(train_dataset, downsampled_size=downsampled_size)
-downsampled_test_dataset = DownsampledCIFAR100(test_dataset, downsampled_size=downsampled_size)
-
 cuda = torch.cuda.is_available()
 
-kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
-train_loader = torch.utils.data.DataLoader(downsampled_train_dataset, batch_size=batch_size, shuffle=True, **kwargs)
-test_loader = torch.utils.data.DataLoader(downsampled_test_dataset, batch_size=batch_size, shuffle=False, **kwargs)
+kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, **kwargs)
 
 
 from torch.optim import lr_scheduler
@@ -62,26 +63,29 @@ from trainer import fit
 from networks import EmbeddingNet, TripletNet, OnlineTripletNet
 from losses import TripletLoss, OnlineTripletLoss
 from utils import AllTripletSelector,HardestNegativeTripletSelector, \
-    RandomNegativeTripletSelector, SemihardNegativeTripletSelector
+    RandomNegativeTripletSelector, SemihardNegativeTripletSelector, RandomTripletSelector
 
 embedding_net = EmbeddingNet(input_depth=input_depth,
                              layer1_stride=layer1_stride,
                              layer1_kernel_size=layer1_kernel_size,
                              layer1_output_channels=layer1_output_channels,
-                             layer1_padding=layer1_padding)
+                             layer1_padding=layer1_padding,
+                             use_relu=use_relu)
 model = OnlineTripletNet(embedding_net)
 if cuda:
     model.cuda()
 loss_fn = OnlineTripletLoss(margin, SemihardNegativeTripletSelector(margin))
+val_loss_fn = OnlineTripletLoss(margin, RandomTripletSelector(margin))
+
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
 # learning rate decay over epochs
-scheduler = lr_scheduler.StepLR(optimizer, 2500, gamma=0.1, last_epoch=-1)
+scheduler = optim.lr_scheduler.StepLR(optimizer, n_epochs // 1.5, gamma=0.1)
 
-fit(train_loader, test_loader, model, loss_fn, optimizer, scheduler, n_epochs, cuda, log_interval, visualize_workings=visualize_model_working)
-
+fit(train_loader, test_loader, model, loss_fn, optimizer, scheduler,
+    n_epochs, cuda, log_interval, visualize_workings=visualize_model_working, val_loss_fn=val_loss_fn)
 if visualize_filter:
-    visualization_filename = "visualization_unsupervised"
+    visualization_filename = "visualization_unsupervised3"
     # Reset
     open(visualization_filename, 'w').close()
 
@@ -91,4 +95,4 @@ if visualize_filter:
                                        filename=visualization_filename)
 
 
-torch.save(model.embedding_net.convnet[0].state_dict(), 'model_unsupervised.pt')
+torch.save(model.embedding_net.convnet[0].state_dict(), 'model_unsupervised3.pt')
