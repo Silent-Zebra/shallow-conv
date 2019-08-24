545df826b94c327928d6f81e8f3c64682a194509
Stephen Zhao <szstephenzhao88@gmail.com>
2019-08-23 23:46:49 -0400

    Try 2 layer conv unsupervised again

--- Experiments_CIFAR100_2Layer.py
+++ Experiments_CIFAR100_2Layer.py
@@ -3,7 +3,7 @@
 # --- HYPERPARAMETERS ---
 
 # image size to downsample to
-downsampled_size = 14
+downsampled_size = 21
 
 batch_size = 512
 
@@ -14,7 +14,7 @@ n_epochs = 80
 # log every x batches
 log_interval = 10
 
-patch_size = 13
+patch_size = 20
 patch_stride = 1
 
 # Convnet hyperparameters
--- visualize_filters_unsupervised.py
+++ visualize_filters_unsupervised.py
@@ -1,6 +1,6 @@
 import utils
 
 # filename = "visualization_unsupervised_16size_4stride"
-filename = "visualization_unsupervised_1l"
+filename = "visualization_unsupervised_2l"
 
 utils.visualize_image_from_file(filename)
