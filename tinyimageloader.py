18de6af77680f244ea558ca482923539d1034d7c
Stephen Zhao <szstephenzhao88@gmail.com>
2019-08-28 18:17:33 -0400

    Try to work with tinyimages

---
+++ tinyimageloader.py
@@ -0,0 +1,51 @@
+import os
+
+import numpy
+import scipy
+
+# paths to various data files
+data_file_path = "~/tiny-images/tiny_images.bin"
+
+# open data files
+data_file = 0
+
+
+def openTinyImage():
+    global data_file
+    data_file = open(data_file_path, "rb")
+
+
+img_count = 79302017
+
+
+def sliceToBin(indx):
+    offset = indx * 3072
+    data_file.seek(offset)
+    data = data_file.read(3072)
+    return numpy.fromstring(data, dtype='uint8')
+
+
+def sliceToImage(data, path):
+    t = data.reshape(32, 32, 3, order="F").copy()
+    img = scipy.misc.toimage(t)
+    img.save(path)
+
+
+def closeTinyImage():
+    data_file.close()
+
+
+
+
+num_images = 800000
+
+
+output_path = "~/tiny-images-subset/"
+
+openTinyImage()
+
+os.mkdir(output_path)
+for i in range(num_images):
+    sliceToImage(sliceToBin(i), output_path+"/"+str(i)+".png")
+
+closeTinyImage()
